a1b61421461d3289a17a9c57f2b2d34d3c2d516f
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-08-22 19:13:21 -0400

    added the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import "./navbar.scss"
-
-export default function Navbar() {
+import {Person, Mail} from "@material-ui/icons"
+export default function Navbar({menuOpen, setMenuOpen}) {
     return (
-        <div className="navbar">
-            
+        <div className={"navbar " + ( menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Lakehal.</a>
+                    <div className="itemContainer"> 
+                    <Person className="icon"/>
+                    <span>+1 407 910 3352</span>
+                    </div>
+                    <div className="itemContainer"> 
+                    <Mail className="icon"/>
+                    <span>contact@khaledlakehal.com</span>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
