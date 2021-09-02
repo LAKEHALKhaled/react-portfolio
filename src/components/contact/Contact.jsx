95287648bd2ea620821e2a9ae2d614c467b6b7d8
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-09-02 18:22:02 -0400

    added contact form and connect it to Emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,27 @@
 import './contact.scss'
 import { useState } from 'react';
 import shake from "../../img/shake.svg";
+import emailjs from "emailjs-com"
+
+
 export default function Contact() {
+
+
+    
 
     const[message, setMessage]= useState(false);
 
-    const handleSubmit = (e)=>{
+    const sendEmail = (e)=>{
     
     e.preventDefault();
+
+    emailjs.sendForm('service_6q0u9l6', 'template_lnul15j', e.target, 'user_6vJZmgwNGuVcT3qvRUbt9')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
     setMessage(true)
 }
 
@@ -18,9 +32,13 @@ export default function Contact() {
             </div>
             <div className="right">
             <h2>Contact Me</h2>
-            <form onSubmit ={handleSubmit}>
-        <input type="text" placeholder="Email" />
-        <textarea placeholder="Message"></textarea>
+
+
+            <form onSubmit ={sendEmail}>
+        <input type="text" placeholder="Name" name="name"/>
+        <input type="text" placeholder="Email" name="email"/>
+        <input type="text" placeholder="Subject" name="subject"/>
+        <textarea placeholder="Message" name="message"></textarea>
         <button type="submit">Send</button>
         {message && <span>Thank you, I will get back to you soon.</span> }
             </form>
